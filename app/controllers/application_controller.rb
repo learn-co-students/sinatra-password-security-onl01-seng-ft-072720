2c52c373aa3480e3df85b8a55c84fab1448e5a22
Your Username <you@example.com>
2020-09-22 15:29:45 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,8 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		if user
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
 		  redirect "/success"
 		else
 		  redirect "/failure"
@@ -40,6 +41,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/success" do
+		# binding.pry
 		if logged_in?
 			erb :success
 		else
